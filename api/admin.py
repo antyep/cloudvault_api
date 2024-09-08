cf1401e83f661057dfab5a37525aa3726146841b
Bunds <antyep6@gmail.com>
2024-09-08 22:47:42 +0200

    feat: register Task, Tag, and TaskTag models in admin

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from .models import Task, Tag, TaskTag
 
 # Register your models here.
+
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ('title', 'status', 'due_date',
+                    'created_at', 'updated_at', 'user')
+    list_filter = ('status', 'due_date', 'user')
+    search_fields = ('title', 'description')
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    search_fields = ('name',)
+
+
+@admin.register(TaskTag)
+class TaskTagAdmin(admin.ModelAdmin):
+    list_display = ('task', 'tag')
