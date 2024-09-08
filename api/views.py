1bae9a966876c74abb78d0940b4b4ab7218687ed
Bunds <antyep6@gmail.com>
2024-09-08 22:26:54 +0200

    feat: add views for task and tag models

--- api/views.py
+++ api/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Task, Tag
+from .serializers import TaskSerializer, TagSerializer
 
 # Create your views here.
+
+
+class TaskViewSet (viewsets.ModelViewSet):
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
+
+
+class TagViewSet(viewsets.ModelViewSet):
+    queryset = Tag.objects.all()
+    serializer_class = TagSerializer
