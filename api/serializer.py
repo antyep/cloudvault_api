4b13353796c8fb3395717a9562bf3d6a795c94d5
Bunds <antyep6@gmail.com>
2024-09-08 22:43:28 +0200

    feat: add serializer for task and tag

---
+++ api/serializer.py
@@ -0,0 +1,14 @@
+from rest_framework import serializers
+from .models import Task, Tag
+
+
+class TaskSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Task
+        fields = '__all__'
+
+
+class TagSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Tag
+        fields = '__all__'
