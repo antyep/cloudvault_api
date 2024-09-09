ede57217d408a7cddf3c595a6e096ea43bfbfdf1
Bunds <antyep6@gmail.com>
2024-09-09 23:52:33 +0200

    fix: update DATABASES configuration for Docker

--- taskflow/settings.py
+++ taskflow/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'taskflow.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'taskflow_db',
+        'USER': 'root',
+        'PASSWORD': 'admin',
+        'HOST': 'db',
+        'PORT': '5432'
     }
 }
 
