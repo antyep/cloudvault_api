0584fd3edf553b7a4bb8894d7e2c3566e97c6cb7
Bunds <antyep6@gmail.com>
2024-09-18 15:50:52 +0200

    feat: add CustomUser and Media models

--- api/models.py
+++ api/models.py
@@ -1,60 +1,34 @@
 from django.db import models
-from django.contrib.auth.models import User
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
 
 
-class Role(models.Model):
-    ROLE_CHOICES = [
-        ('admin', 'Administrator'),
-        ('mod', 'Moderator'),
-        ('user', 'User'),
-    ]
+class CustomUser(AbstractUser):
 
-    name = models.CharField(
-        max_length=20, choices=ROLE_CHOICES, default='user')
+    id = models.AutoField(primary_key=True)
+    email = models.EmailField(unique=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True, null=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
-        return self.name
+        return self.username
 
 
-class CustomUser(User):
-    role = models.ForeignKey(
-        Role, on_delete=models.SET_NULL, null=True, blank=True)
+class Media(models.Model):
 
-
-def __str__(self):
-    return self.username
-
-
-class Task(models.Model):
-    STATUS_CHOICES = [
-        ('pending', 'Pending'),
-        ('in_progress', 'In Progress'),
-        ('completed', 'Completed')
-    ]
-
-    title = models.CharField(max_length=180)
-    description = models.TextField(blank=True)
-    status = models.CharField(
-        max_length=20, choices=STATUS_CHOICES, default='pending')
-    due_date = models.DateTimeField(blank=True, null=True)
+    id = models.AutoField(primary_key=True)
+    title = models.CharField(max_length=255)
+    description = models.TextField(null=True, blank=True)
+    media_file = models.FileField(upload_to='media/')
+    file_type = models.CharField(max_length=50)
+    file_size = models.IntegerField()
+    user = models.ForeignKey(
+        CustomUser, on_delete=models.CASCADE, related_name='media_files')
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    user = models.ForeignKey(
-        User, on_delete=models.CASCADE, related_name='tasks')
+    is_public = models.BooleanField(default=False)
+    is_deleted = models.BooleanField(default=False)
 
     def __str__(self):
         return self.title
-
-
-class Tag(models.Model):
-    name = models.CharField(max_length=100, unique=True)
-
-    def __str__(self):
-        return self.name
-
-
-class TaskTag(models.Model):
-    task = models.ForeignKey(Task, on_delete=models.CASCADE)
-    tag = models.ForeignKey(Tag, on_delete=models.CASCADE)
