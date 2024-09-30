5515f4744ddd9b437bfff4313af1095dc2b37700
antyep <antyep6@gmail.com>
2024-09-30 13:54:08 +0200

    feat: add loginview to urls.py

--- cloudvault/urls.py
+++ cloudvault/urls.py
@@ -16,11 +16,21 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from api.views import LoginView
 from rest_framework.documentation import include_docs_urls
+
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
     path('docs/', include_docs_urls(title='API Documentation')),
     path('accounts/', include('allauth.urls')),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('login/', LoginView.as_view(), name='login'),
+
 ]
