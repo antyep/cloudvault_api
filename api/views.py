ea676040bce9906580c206fcd89fc8a6d1a81d7a
antyep <antyep6@gmail.com>
2024-09-30 13:53:26 +0200

    feat: add login view to views.py

--- api/views.py
+++ api/views.py
@@ -1,23 +1,23 @@
 from rest_framework import viewsets
 from .models import CustomUser, Media
 from .serializer import CustomUserSerializer, MediaSerializer
-from django.contrib.auth import login
-from rest_framework import permissions
-from rest_framework.authtoken.serializers import AuthTokenSerializer
-from knox.views import LoginView as KnoxLoginView
+from rest_framework import generics
+from rest_framework.response import Response
+from rest_framework import status
+from .serializer import LoginSerializer
 
 # Create your views here.
 
 
-class LoginView(KnoxLoginView):
-    permission_classes = (permissions.AllowAny,)
+class LoginView(generics.GenericAPIView):
+    serializer_class = LoginSerializer
 
-    def post(self, request, format=None):
-        serializer = AuthTokenSerializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        user = serializer.validated_data['user']
-        login(request, user)
-        return super(LoginView, self).post(request, format=None)
+    def post(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        if serializer.is_valid():
+            user_data = serializer.create(serializer.validated_data)
+            return Response(user_data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 class CustomUserViewSet (viewsets.ModelViewSet):
