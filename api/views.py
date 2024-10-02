78b6dd2541fb737b9b6340a8880c04799210585d
antyep <antyep6@gmail.com>
2024-10-02 09:22:55 +0200

    feat: add smtp view to views.py (resend)

--- api/views.py
+++ api/views.py
@@ -1,9 +1,36 @@
 from rest_framework import viewsets
 from .models import CustomUser, Media
 from .serializer import CustomUserSerializer, MediaSerializer
-
+from django.http import JsonResponse
+from django.core.mail import EmailMessage, get_connection
+from django.conf import settings
+import os
 
 # Create your views here.
+
+
+def index(request):
+
+    subject = "CloudVault Validation"
+    recipient_list = ["delivered@resend.dev"]
+    from_email = "onboarding@resend.dev"
+    message = "<strong>test</strong>"
+
+    with get_connection(
+        host=settings.RESEND_SMTP_HOST,
+        port=settings.RESEND_SMTP_PORT,
+        username=settings.RESEND_SMTP_USERNAME,
+        password=os.environ["RESEND_API_KEY"],
+        use_tls=True,
+    ) as connection:
+        r = EmailMessage(
+            subject=subject,
+            body=message,
+            to=recipient_list,
+            from_email=from_email,
+            connection=connection).send()
+        return JsonResponse({"status": "ok"})
+
 
 class CustomUserViewSet (viewsets.ModelViewSet):
     queryset = CustomUser.objects.all()
