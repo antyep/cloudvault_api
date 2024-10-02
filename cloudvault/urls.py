581bbcf2c957a04e5baa703e7d499708f1de3e6d
antyep <antyep6@gmail.com>
2024-10-02 09:23:35 +0200

    feat: add simplejwt urls to urls.py

--- cloudvault/urls.py
+++ cloudvault/urls.py
@@ -18,10 +18,18 @@ from django.contrib import admin
 from django.urls import path, include
 from rest_framework.documentation import include_docs_urls
 
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
     path('docs/', include_docs_urls(title='API Documentation')),
     path('accounts/', include('allauth.urls')),
     path("_allauth/", include("allauth.headless.urls")),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
